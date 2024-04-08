a2a09dd7ff0d2cd0614ab054310601ee808c065d
SILVA, H. N <hudsonnsilva@gmail.com>
2024-04-08 20:55:20 -0300

    Add files via upload

---
+++ 07.InsercaoTabela_tipo_transacao.sql
@@ -0,0 +1,5 @@
+-- Inserindo registos na tabela tipo transação
+INSERT INTO tipo_transacao (nome) VALUES ('Débito');
+INSERT INTO tipo_transacao (nome) VALUES ('Crédito');
+INSERT INTO tipo_transacao (nome) VALUES ('Transferência');
+INSERT INTO tipo_transacao (nome) VALUES ('Empréstimo');
