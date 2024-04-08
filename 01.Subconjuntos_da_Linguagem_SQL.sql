2c51a1d1be092b79fcc5318eed2173f953d4a61c
SILVA, H. N <hudsonnsilva@gmail.com>
2024-04-08 20:36:31 -0300

    Add files via upload

---
+++ 01.Subconjuntos_da_Linguagem_SQL.sql
@@ -0,0 +1,31 @@
+-- LINGUAGEM DE CONSULTA ESTRUTURADA - SQL
+-- Padronização SQL
+-- Subconjuntos da Linguagem SQL
+/*
+	DML - Linguagem de manipulação de dados
+	UPDATE - usado para alterar dados
+	SELECT - Consulta de dados
+	DELETE - excluir registros
+	INSERT - usado para inserir dados
+*/
+/*
+	DDL - Linguagem de definição de dados
+	ALTER - permite alterar um objeto do banco
+	CREATE - permite criar objetos
+	DROP - permite apagar um objeto do banco	
+*/
+/*
+	DCL - Linguagem de controle de dados
+	GRANT - concede privilégios
+	REVOKE - remove privilégios
+*/
+/*
+	DTL - Linguagem de transação de dados
+	BEGIN - marca um inicio da transação
+	COMMIT - encerra uma transação
+	ROLLBACK - encerra a transação, desfazendo todas as operação
+*/
+/*
+	DQL - Linguagem de consulta de dados
+	SELECT - consulta
+*/
